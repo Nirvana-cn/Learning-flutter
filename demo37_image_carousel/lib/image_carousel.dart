f75184f236b8db7fe68e4902670547d411a7c6ad
Nirvana <574014131@qq.com>
2020-06-09 18:48:13 +0800

    @dev ImageCarousel

---
+++ demo37_image_carousel/lib/image_carousel.dart
@@ -0,0 +1,112 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+
+const _ZERO_INDEX = 0;
+const _DEFAULT_BEGIN = 1;
+const _DEFAULT_TIME = Duration(seconds: 1);
+
+class ImageCarousel extends StatefulWidget {
+  final Duration transitionTime;
+  final Duration stayTime;
+  final List<Widget> children;
+  final bool reverse;
+  final Axis scrollDirection;
+  final Curve curve;
+
+  ImageCarousel({
+    Key key,
+    @required this.children,
+    this.transitionTime = _DEFAULT_TIME,
+    this.stayTime = _DEFAULT_TIME,
+    this.reverse = false,
+    this.scrollDirection = Axis.horizontal,
+    this.curve = Curves.linearToEaseOut,
+  }) : super(key: key);
+
+  @override
+  _ImageCarouselState createState() => _ImageCarouselState();
+}
+
+class _ImageCarouselState extends State<ImageCarousel> {
+  int defaultEnd;
+  PageController controller;
+  Timer timer;
+  int index;
+
+  @override
+  void initState() {
+    super.initState();
+    index = _DEFAULT_BEGIN;
+    defaultEnd = widget.children.length;
+    controller = PageController(initialPage: index);
+
+    initTimer();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    List<Widget> widgets = generateWidgets();
+
+    return NotificationListener(
+      onNotification: (notification) {
+        switch (notification.runtimeType) {
+          case ScrollEndNotification:
+            if (index == _ZERO_INDEX) {
+              index = defaultEnd;
+              controller.jumpToPage(index);
+            }
+            if (index == widgets.length - 1) {
+              index = _DEFAULT_BEGIN;
+              controller.jumpToPage(index);
+            }
+            break;
+        }
+        return true;
+      },
+      child: Listener(
+        onPointerDown: (downEvent) {
+          if (timer.isActive) {
+            timer.cancel();
+          }
+        },
+        onPointerUp: (upEvent) {
+          initTimer();
+        },
+        child: PageView(
+          scrollDirection: widget.scrollDirection,
+          controller: controller,
+          reverse: widget.reverse,
+          physics: const BouncingScrollPhysics(),
+          children: widgets,
+          onPageChanged: (int currentPage) {
+            index = currentPage;
+          },
+        ),
+      ),
+    );
+  }
+
+  List<Widget> generateWidgets() {
+    List<Widget> widgets = [];
+    Widget firstElement = widget.children.first;
+    Widget lastElement = widget.children.last;
+    widgets.add(lastElement);
+    widgets.addAll(widget.children);
+    widgets.add(firstElement);
+
+    return widgets;
+  }
+
+  void initTimer() {
+    timer = Timer.periodic(widget.stayTime + widget.transitionTime, (t) {
+      controller.animateToPage(++index, duration: widget.transitionTime, curve: widget.curve);
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    timer.cancel();
+    controller.dispose();
+  }
+}
---
+++ demo37_image_carousel/lib/main.dart
@@ -0,0 +1,66 @@
+import 'package:demo37imagecarousel/image_carousel.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: ' ImageCarousel'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Container(
+          width: 300,
+          height: 200,
+          child: ImageCarousel(
+            children: [
+              FadeInImage(
+                width: 300,
+                height: 300,
+                fit: BoxFit.cover,
+                fadeInDuration: Duration(seconds: 1),
+                placeholder: AssetImage("images/game.png"),
+                image: NetworkImage("https://images.unsplash.com/photo-1503803548695-c2a7b4a5b875?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"),
+              ),
+              Container(
+                color: Colors.green,
+              ),
+              Container(
+                color: Colors.amber,
+              ),
+              Container(
+                color: Colors.blueAccent,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
